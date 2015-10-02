deacab4b2ed98fa2f217ff76f6b8f1662f895e73
thomasweng15 <thomas.weng11@gmail.com>
2015-10-02 03:11:53 -0400

    Added Tumblr authentication, edited README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# cup
-ctp
+# ctp
+
+## To Do
+* Index file to track pictures already uploaded
+* Log file of tasks 
+* Integrate Tumblr API
+
+## Done
+* Set up cron job
+* Tumblr API Authentication
+
+## Dependencies
+* pytumblr `sudo easy_install pytumblr`
--- ctp.py
+++ ctp.py
@@ -1,19 +1,27 @@
-
-# To Do
-# Timer functionality using cron
-# Plaintext file of pictures/indices already uploaded
-#   On launch, check file 
-# Log file
-# Tumblr API
+#!/usr/bin/python
 
 import sys
 import argparse
+import pytumblr
 
 class CTP():
-	def __init__(self, logf, indexf):
-		self.logf = open(logf, 'a')
+	def __init__(self, indexf):
 		self.indexf = open(indexf, 'a')	
-		# call function to init Tumblr API access	
+
+		self.api_key = 'VABuQIpKd3HPGJlXUVtA4nLOjAkgfzfzhGAIvrh5AYH15b1lLK'
+		self.api_secret = 'eTCnWarGzAyn5oIIZUkSHt7JdIUpYzS5lq7oZTx7zz4TjVTiH8'
+		self.oauth_token = 'tHZItSfPprgbXHsDDx1MgzGolv0IMwc6ccfd3MrAiH3ZTbqRGR'
+		self.oauth_secret = 'OlvCCgAwwtSiIJTHcE6nMINORFcenDx92D6X8u1sSAfwdE3xa9' 
+		
+		# Authenticate via OAuth 
+		self.client = pytumblr.TumblrRestClient(
+			self.api_key, 
+			self.api_secret, 
+			self.oauth_token,
+			self.oauth_secret) 
+ 
+		# Make the request 
+		print self.client.info()	
 
 	def cleanup(self):
 		self.logf.close()
@@ -21,10 +29,9 @@ class CTP():
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Tumblr photo poster')
-	parser.add_argument('-l', help='log file', default='log.txt')
 	parser.add_argument('-i', help='index file', default='index.txt')
 	args = parser.parse_args()
 
-	ctp = CTP(args.l, args.i)
+	ctp = CTP(args.i)
 	ctp.cleanup()
 
