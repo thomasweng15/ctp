3fdd417205c0e6b5a4b38909eb32f5f2ae258b33
Thomas Weng <thomas.weng11@gmail.com>
2015-10-05 01:58:12 -0700

    Can post text using pytumblr

--- ctp.py
+++ ctp.py
@@ -6,21 +6,26 @@ import pytumblr
 
 class CTP():
 	def __init__(self, indexf):
-		self.indexf = open(indexf, 'a')	
+		self.indexf = open(indexf, 'a')
 
 		self.api_key = 'VABuQIpKd3HPGJlXUVtA4nLOjAkgfzfzhGAIvrh5AYH15b1lLK'
 		self.api_secret = 'eTCnWarGzAyn5oIIZUkSHt7JdIUpYzS5lq7oZTx7zz4TjVTiH8'
 		self.oauth_token = 'tHZItSfPprgbXHsDDx1MgzGolv0IMwc6ccfd3MrAiH3ZTbqRGR'
-		self.oauth_secret = 'OlvCCgAwwtSiIJTHcE6nMINORFcenDx92D6X8u1sSAfwdE3xa9' 
-		# Authenticate via OAuth 
+		self.oauth_secret = 'OlvCCgAwwtSiIJTHcE6nMINORFcenDx92D6X8u1sSAfwdE3xa9'
+
 		self.client = pytumblr.TumblrRestClient(
-			self.api_key, 
-			self.api_secret, 
+			self.api_key,
+			self.api_secret,
 			self.oauth_token,
-			self.oauth_secret) 
- 
-		# Make the request 
-		print self.client.info()	
+			self.oauth_secret)
+
+	def post(self):
+		print self.client.create_text(
+			"chihiroandthomas",
+			state="published",
+			slug="testing-text-posts",
+			title="Testing",
+			body="testing1 2 3 4")
 
 	def cleanup(self):
 		self.indexf.close()
@@ -31,5 +36,5 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	ctp = CTP(args.i)
+	ctp.post()
 	ctp.cleanup()
-
